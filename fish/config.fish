7a9d8417fe4009f5b96cffd6d70a2183fd408121
duartelcortes@gmail.com <duartelcortes@gmail.com>
2025-10-01 20:39:35 +0100

    fish and foot

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,11 @@
 if status is-interactive
-    # Starship custom prompt
-    starship init fish | source
-
+    set -g fish_greeting ""
     # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
 
     # For jumping between prompts in foot terminal
-    function mark_prompt_start --on-event fish_prompt
-        echo -en "\e]133;A\e\\"
-    end
+    #function mark_prompt_start --on-event fish_prompt
+    echo -en "\e]133;A\e\\"
+    fastfetch
+    #end
 end
