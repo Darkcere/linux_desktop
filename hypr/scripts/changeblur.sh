b9bdfc5556e87c72fe6730a732af42b6ff602522
duartelcortes@gmail.com <duartelcortes@gmail.com>
2025-11-13 17:44:44 +0000

    Change blur

---
+++ hypr/scripts/changeblur.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Script for changing blurs on the fly
+
+notif="$HOME/.config/swaync/images/bell.png"
+
+STATE=$(hyprctl -j getoption decoration:blur:enabled | jq ".int")
+
+if [ "${STATE}" == "1" ]; then
+  hyprctl keyword decoration:blur:enabled 0
+  notify-send -e -u low -i "$notif" "No blur"
+else
+  hyprctl keyword decoration:blur:enabled 1
+  notify-send -e -u low -i "$notif" "Normal blur"
+fi
